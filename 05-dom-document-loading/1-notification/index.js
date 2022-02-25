f9a76cf5c6c7f932420a32c318e3d7ccb037865d
m-evstifeev <evstifeevmaks@gmail.com>
2022-02-25 14:00:45 +0400

    Added NotificationMessage component

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,60 @@
 export default class NotificationMessage {
+  static previousNotification = null;
 
+  constructor(
+    text = '',
+    {
+      duration = 0, 
+      type = ''
+    } = {}) {
+    this.text = text;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  render() {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = this.getNotificationTemplate();
+
+    this.element = wrapper.firstElementChild;
+  }
+
+  getNotificationTemplate() {
+    return `<div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+              <div class="timer"></div>
+                <div class="inner-wrapper">
+                  <div class="notification-header">${this.type}</div>
+                  <div class="notification-body">
+                    ${this.text}
+                  </div>
+                </div>
+            </div>`
+  }
+
+  show(wrapper) {
+    let targetElement = document.body;
+
+    if (NotificationMessage.previousElement) {
+      NotificationMessage.previousElement.remove();
+    }
+
+    if(wrapper) {
+      targetElement = wrapper;
+    }
+    
+    targetElement.append(this.element);
+    setTimeout(this.remove.bind(this), this.duration);
+
+    NotificationMessage.previousElement = this;
+  }
+
+  remove () {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
